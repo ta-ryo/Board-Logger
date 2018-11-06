e7dbc68971845828870bd6a95b7737de87bd7d80
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2018-11-06 22:46:41 +0900

    Change the writing of the environment variable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_11_02_030037) do
-ActiveRecord::Schema.define(version: 2018_11_02_025632) do
 
   create_table "boards", force: :cascade do |t|
     t.string "location"
@@ -37,18 +36,19 @@ ActiveRecord::Schema.define(version: 2018_11_02_025632) do
     t.index ["board_id"], name: "index_maps_on_board_id"
   end
 
+  create_table "moods", force: :cascade do |t|
+    t.string "mood"
+    t.integer "board_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "photos", force: :cascade do |t|
     t.string "image"
     t.integer "manner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["manner_id"], name: "index_photos_on_manner_id"
-    
-  create_table "moods", force: :cascade do |t|
-    t.string "mood"
-    t.integer "board_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "waves", force: :cascade do |t|
